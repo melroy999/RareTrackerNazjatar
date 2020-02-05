c9f4b10217c5ef128184ec7ead4b79000986ea6d
Melroy <m.v.nijnatten@student.tue.nl>
2020-02-05 19:22:10 +0100

    [/master] Added the missing English localization strings. Added the new (but still missing) strings to the French and Chinese localizations.

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -4,6 +4,13 @@ if not L then return end
 
 -- Option menu strings.
 L["Rare window scale"] = "稀有窗口缩放"
+-- L["Set the scale of the rare window."] = ""
 L["Disable All"] = "禁用全部"
+-- L["Disable all non-favorite rares in the list."] = ""
 L["Enable All"] = "启用全部"
+-- L["Enable all rares in the list."] = ""
 L["Reset Favorites"] = "重置偏好"
+L["Reset the list of favorite rares."] = ""
+-- L["General Options"] = ""
+-- L["Rare List Options"] = ""
+-- L["Active Rares"] = ""
